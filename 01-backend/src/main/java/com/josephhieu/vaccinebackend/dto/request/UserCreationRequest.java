3a61ac76e5b2e982618c595aa8ff6a30e30184df
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2026-01-17 17:11:51 +0700

    Update Validate for backend when create user

--- 01-backend/src/main/java/com/josephhieu/vaccinebackend/dto/request/UserCreationRequest.java
+++ 01-backend/src/main/java/com/josephhieu/vaccinebackend/dto/request/UserCreationRequest.java
@@ -1,6 +1,8 @@
 package com.josephhieu.vaccinebackend.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,11 +18,13 @@ import lombok.NoArgsConstructor;
 public class UserCreationRequest {
 
     /** Tên đăng nhập của nhân viên. */
-    @NotBlank(message = "Vui lòng nhập đầy đủ thông tin.") // Thông báo lỗi theo SRS
+    @NotBlank(message = "Vui lòng nhập đầy đủ thông tin.") // Thông báo lỗi
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Tên đăng nhập không hợp lệ.")
     private String tenDangNhap;
 
     /** Mật khẩu khởi tạo. */
     @NotBlank(message = "MISSING_INFO")
+    @Size(min = 6, message = "Mật khẩu phải từ 6 ký tự trở lên.")
     private String matKhau;
 
     /** * Mã quyền hạn (UUID).
@@ -35,6 +39,7 @@ public class UserCreationRequest {
 
     /** Số chứng minh nhân dân/CCCD. */
     @NotBlank(message = "MISSING_INFO")
+    @Pattern(regexp = "^[0-9]{9,12}$", message = "Số CMND không đúng định dạng.")
     private String cmnd;
 
     /** Nơi ở hiện tại của nhân viên. */
